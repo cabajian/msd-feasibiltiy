852a3ba0c12585567b82ba01930dc152449814fb
cabajian <cxa6282@rit.edu>
2021-07-26 15:16:31 -0400

    Initial Mbed Studio blank bare-metal program commit.

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+ * File    : main.cpp
+ * Author  : Chris Abajian (cxa6282@rit.edu)
+ * Created : July 26, 2021
+ *
+ * Description:
+ *   Initial feasibility program for testing the K64F MCU and sensors
+ *   established in the Preliminary Detailed Design phase of RIT MSD I.
+ */
+ 
+ #include "mbed.h"
+
+
+/*
+ * Main function.
+ */
+int main()
+{
+    while (true) {
+
+    }
+}
