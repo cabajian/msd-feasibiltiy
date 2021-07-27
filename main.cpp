8bf594c4fda9a78de20afe8de2527143eda8e279
cabajian <abajianchris@gmail.com>
2021-07-27 18:32:26 -0400

    Added initial I2C instnatiation to main().

--- main.cpp
+++ main.cpp
@@ -10,13 +10,23 @@
  
  #include "mbed.h"
 
+I2C i2c(I2C_SDA, I2C_SCL); //PTE25,PTE24
+
+#define LSM6DSOX_ADDR  (0x6A << 1)
+#define LSM6DSOX_WHO_AM_I_ADDR  (0x0F)
 
 /*
- * Main function.
+ * Main function.   
  */
-int main()
-{
+int main() {
+    char data[1];
+    char cmd[1];
+    i2c.frequency(100000);
     while (true) {
-
+        cmd[0] = {LSM6DSOX_WHO_AM_I_ADDR};
+        i2c.write(LSM6DSOX_ADDR, cmd, 1, true);
+        i2c.read(LSM6DSOX_ADDR, data, 1);
+        printf("WHO_AM_I: %x\n", data[0]);
+        ThisThread::sleep_for(5s);
     }
 }
