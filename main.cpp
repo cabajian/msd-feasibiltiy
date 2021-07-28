68a8c5026b4f366e66aaaeb2b8729539cfa76b85
cabajian <abajianchris@gmail.com>
2021-07-27 21:27:09 -0400

    Implemented basic LSM6DSOX accelerometer readouts via I2C.

--- main.cpp
+++ main.cpp
@@ -9,24 +9,54 @@
  */
  
  #include "mbed.h"
+#include <cstdint>
 
-I2C i2c(I2C_SDA, I2C_SCL); //PTE25,PTE24
+I2C i2c(PTE25, PTE24); //SDA,SCL
 
-#define LSM6DSOX_ADDR  (0x6A << 1)
+#define LSM6DSOX_ADDR           (0x6A<<1)
 #define LSM6DSOX_WHO_AM_I_ADDR  (0x0F)
+#define LSM6DSOX_CTRL1_XL_ADDR  (0x10)
+#define LSM6DSOX_CTRL8_XL_ADDR  (0x17)
+#define LSM6DSOX_OUTX_L_A_ADDR  (0x28)
 
 /*
  * Main function.   
  */
 int main() {
-    char data[1];
-    char cmd[1];
+    char data[6];
+    char cmd[2];
     i2c.frequency(100000);
+
+    /* WHO_AM_I printout */
+    cmd[0] = {LSM6DSOX_WHO_AM_I_ADDR};
+    i2c.write(LSM6DSOX_ADDR, &cmd[0], 1);
+    i2c.read(LSM6DSOX_ADDR, &data[0], 1);
+    printf("WHO_AM_I: %x\n", data[0]);
+
+    /* Accelerometer config */
+    // CTRL1_XL (Accelerometer control register 1)
+    cmd[0] = LSM6DSOX_CTRL1_XL_ADDR;
+    cmd[1] = 0xA6; //6.66kHz, +-16g, hi-res
+    i2c.write(LSM6DSOX_ADDR, cmd, 2);
+    // CTRL8_XL (Control register 8)
+    cmd[0] = LSM6DSOX_CTRL8_XL_ADDR;
+    i2c.write(LSM6DSOX_ADDR, &cmd[0], 1);
+    i2c.read(LSM6DSOX_ADDR, &data[0], 1);
+    cmd[1] = data[0] & ~0x02; //clear XL_FS_MODE to retain full +-16g scale
+    i2c.write(LSM6DSOX_ADDR, cmd, 2);
+
     while (true) {
-        cmd[0] = {LSM6DSOX_WHO_AM_I_ADDR};
-        i2c.write(LSM6DSOX_ADDR, cmd, 1, true);
-        i2c.read(LSM6DSOX_ADDR, data, 1);
-        printf("WHO_AM_I: %x\n", data[0]);
+        /* Read accelerometer */
+        // CTRL8_XL (Control register 8)
+        cmd[0] = LSM6DSOX_OUTX_L_A_ADDR;
+        i2c.write(LSM6DSOX_ADDR, &cmd[0], 1);
+        i2c.read(LSM6DSOX_ADDR, data, 6); //read all 6 bytes (XYZ regs, 16b each)
+        int16_t acc_x = (data[0] << 8 | data[1]);
+        int16_t acc_y = (data[2] << 8 | data[3]);
+        int16_t acc_z = (data[4] << 8 | data[5]);
+        printf("X: %f\n",   (acc_x / 2048.0f));
+        printf("Y: %f\n",   (acc_y / 2048.0f));
+        printf("Z: %f\n\n", (acc_z / 2048.0f));
         ThisThread::sleep_for(5s);
     }
 }
