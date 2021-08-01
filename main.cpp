9e06f7ae7a3515269fa8c26f36ac2fcecab47650
cabajian <abajianchris@gmail.com>
2021-08-01 17:54:31 -0400

    Completed program to read LSM6DSOX sensor.

--- main.cpp
+++ main.cpp
@@ -9,9 +9,7 @@
  */
  
 #include "mbed.h"
-// #include <cstdint>
 #include "msd.h"
-#include <algorithm>
 
 I2C i2c(PTE25, PTE24); //SDA,SCL
 
@@ -19,66 +17,112 @@ I2C i2c(PTE25, PTE24); //SDA,SCL
  * Main function.   
  */
 int main() {
-    char data[6];
-    char cmd[2];
-    i2c.frequency(100000);
+    char data[14];
+    i2c.frequency(400000);
 
     /* WHO_AM_I printout */
-    cmd[0] = {LSM6DSOX_WHO_AM_I_ADDR};
-    i2c.write(LSM6DSOX_ADDR, &cmd[0], 1);
-    i2c.read(LSM6DSOX_ADDR, &data[0], 1);
+    readReg(LSM6DSOX_ADDR, LSM6DSOX_WHO_AM_I_ADDR, data, 1);
     printf("WHO_AM_I: %x\n", data[0]);
+
+    /* LSM6DSOX config */
+    // CTRL3_C (Control register 3)
+    //   set self-clearing reset bit
+    setBits(LSM6DSOX_ADDR, LSM6DSOX_CTRL3_C_ADDR, 0x01);
+    readReg(LSM6DSOX_ADDR, LSM6DSOX_CTRL3_C_ADDR, data, 1);
+    while(data[0] & 0x01) {
+        wait_us(100);
+        // Read until reset bit clears
+        readReg(LSM6DSOX_ADDR, LSM6DSOX_CTRL3_C_ADDR, data, 1);
+    }
+    // CTRL3_C (Control register 3)
+    //   set BDU to only update output regs after reading
+    setBits(LSM6DSOX_ADDR, LSM6DSOX_CTRL3_C_ADDR, 0x40);
 
     /* Accelerometer config */
     // CTRL1_XL (Accelerometer control register 1)
-    cmd[0] = LSM6DSOX_CTRL1_XL_ADDR;
-    cmd[1] = 0xA6; //6.66kHz, +-16g, hi-res
-    i2c.write(LSM6DSOX_ADDR, cmd, 2);
+    //   416Hz, +-8g
+    writeReg(LSM6DSOX_ADDR, LSM6DSOX_CTRL1_XL_ADDR, 0x6A);
+    double acc_sens = 0.244; // 0.244 milli-g per bit in +-8g scale
     // CTRL8_XL (Control register 8)
-    cmd[0] = LSM6DSOX_CTRL8_XL_ADDR;
-    i2c.write(LSM6DSOX_ADDR, &cmd[0], 1);
-    i2c.read(LSM6DSOX_ADDR, &data[0], 1);
-    cmd[1] = data[0] & ~0x02; //clear XL_FS_MODE to retain full +-16g scale
-    i2c.write(LSM6DSOX_ADDR, cmd, 2);
+    //   clear XL_FS_MODE to retain full +-16g scale
+    clearBits(LSM6DSOX_ADDR, LSM6DSOX_CTRL8_XL_ADDR, 0x02);
+    // CTRL9_XL (Control register 9)
+    //   set "I3C disable" bit since we aren't using it
+    setBits(LSM6DSOX_ADDR, LSM6DSOX_CTRL9_XL_ADDR, 0x01);
+
+    /* Gyroscope config */
+    // CTRL2_G (Gyroscope control register 2)
+    //   416Hz, 2000dps
+    writeReg(LSM6DSOX_ADDR, LSM6DSOX_CTRL2_G_ADDR, 0x6C);
+    double gyr_sens = 70.0; // 70.0 milli-dps per bit in 2000dps scale
+
 
     while (true) {
-        /* Read accelerometer */
-        // CTRL8_XL (Control register 8)
-        cmd[0] = LSM6DSOX_OUTX_L_A_ADDR;
-        i2c.write(LSM6DSOX_ADDR, &cmd[0], 1);
-        i2c.read(LSM6DSOX_ADDR, data, 6); //read all 6 bytes (XYZ regs, 16b each)
-        int16_t acc_x = (data[0] << 8 | data[1]);
-        int16_t acc_y = (data[2] << 8 | data[3]);
-        int16_t acc_z = (data[4] << 8 | data[5]);
-        printf("X: %f\n",   (acc_x / 2048.0f));
-        printf("Y: %f\n",   (acc_y / 2048.0f));
-        printf("Z: %f\n\n", (acc_z / 2048.0f));
-        ThisThread::sleep_for(5s);
+        /* Read gyro/accel data */
+        readReg(LSM6DSOX_ADDR, LSM6DSOX_OUT_TEMP_L_ADDR, data, 14);
+        int16_t temp_i = (data[1] << 8 | data[0]);
+        double  temp_d = (temp_i/256.0) + 25.0;
+        int16_t gyr_ix = (data[3] << 8 | data[2]);
+        int16_t gyr_iy = (data[5] << 8 | data[4]);
+        int16_t gyr_iz = (data[7] << 8 | data[6]);
+        double  gyr_dx = gyr_ix * gyr_sens * SENSORS_DPS_TO_RADS / 1000;
+        double  gyr_dy = gyr_iy * gyr_sens * SENSORS_DPS_TO_RADS / 1000;
+        double  gyr_dz = gyr_iz * gyr_sens * SENSORS_DPS_TO_RADS / 1000;
+        int16_t acc_ix = (data[9]  << 8 | data[8]);
+        int16_t acc_iy = (data[11] << 8 | data[10]);
+        int16_t acc_iz = (data[13] << 8 | data[12]);
+        double  acc_dx = acc_ix * acc_sens * SENSORS_GRAVITY_STANDARD / 1000;
+        double  acc_dy = acc_iy * acc_sens * SENSORS_GRAVITY_STANDARD / 1000;
+        double  acc_dz = acc_iz * acc_sens * SENSORS_GRAVITY_STANDARD / 1000;
+        printf("Temp: %f\n",   temp_d);
+        printf("GyrX: %f\n",   gyr_dx);
+        printf("GyrY: %f\n",   gyr_dy);
+        printf("GyrZ: %f\n",   gyr_dz);
+        printf("AccX: %f\n",   acc_dx);
+        printf("AccY: %f\n",   acc_dy);
+        printf("AccZ: %f\n\n", acc_dz);
+        ThisThread::sleep_for(100ms);
+        // wait_us(150);
     }
 }
 
 
 /*
- * I2C Read function.   
+ * I2C Read helper function.   
  */
-void readReg(int address, int subaddress, const char *data, int length = 1) {
+void readReg(int address, uint8_t subaddress, char *data, int length = 1) {
     // Set register to read
-    const char subaddr[1] = {(char)subaddress};
+    char subaddr[1] = {subaddress};
     i2c.write(address, subaddr, 1);
     // Read register
-    char rdata[length+1];
-    rdata[0] = (char)subaddress;
-    std::copy(&data[0], &data[length-1], &rdata[1]);
-    i2c.read(address, rdata, length+1);
+    i2c.read(address, data, length);
 }
 
 /*
- * I2C Write function.    
+ * I2C Write helper function.    
  */
-void writeReg(int address, int subaddress, const char *data, int length = 1) {
+void writeReg(int address, uint8_t subaddress, uint8_t command) {
     // Write register
-    char wdata[length+1];
-    wdata[0] = (char)subaddress;
-    std::copy(&data[0], &data[length-1], &wdata[1]);
-    int res1 = i2c.write(address, wdata, length+1);
+    char wdata[2] = {subaddress, command};
+    i2c.write(address, wdata, 2);
+}
+
+/*
+ * I2C Set bits function.    
+ */
+void setBits(int address, uint8_t subaddress, uint8_t mask) {
+    char data[1];
+    readReg(address, subaddress, data, 1);
+    data[0] |= mask;
+    writeReg(address, subaddress, data[0]);
+}
+
+/*
+ * I2C Clear bits function.    
+ */
+void clearBits(int address, uint8_t subaddress, uint8_t mask) {
+    char data[1];
+    readReg(address, subaddress, data, 1);
+    data[0] &= ~mask;
+    writeReg(address, subaddress, data[0]);
 }
--- msd.h
+++ msd.h
@@ -12,14 +12,21 @@
 #define MSD_INCLUDE_H
 
 /* I2C read/write functions */
-void readReg(int address, int subaddress, const char *data, int length);
-void writeReg(int address, int subaddress, const char *data, int length);
+void readReg(int address, uint8_t subaddress, char *data, int length);
+void writeReg(int address, uint8_t subaddress, uint8_t command);
+void setBits(int address, uint8_t subaddress, uint8_t mask);
+void clearBits(int address, uint8_t subaddress, uint8_t mask);
 
 /* LSM6DSOX macros */
 #define LSM6DSOX_ADDR           (0x6A<<1)
 #define LSM6DSOX_WHO_AM_I_ADDR  (0x0F)
 #define LSM6DSOX_CTRL1_XL_ADDR  (0x10)
+#define LSM6DSOX_CTRL2_G_ADDR   (0x11)
+#define LSM6DSOX_CTRL3_C_ADDR   (0x12)
 #define LSM6DSOX_CTRL8_XL_ADDR  (0x17)
+#define LSM6DSOX_CTRL9_XL_ADDR  (0x18)
+#define LSM6DSOX_OUT_TEMP_L_ADDR (0x20)
+#define LSM6DSOX_OUTX_L_G_ADDR  (0x22)
 #define LSM6DSOX_OUTX_L_A_ADDR  (0x28)
 
 #endif
